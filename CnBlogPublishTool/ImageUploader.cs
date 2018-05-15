5ee732ab3722bc1ed2c35eb5624ecbfcaf0a27fa
xcmaster <stulzq@qq.com>
2018-05-15 21:55:32 +0800

    加入重试机制

--- CnBlogPublishTool/ImageUploader.cs
+++ CnBlogPublishTool/ImageUploader.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using CnBlogPublishTool.Util;
 using MetaWeblogClient;
+using Polly;
 
 namespace CnBlogPublishTool
 {
@@ -16,9 +17,26 @@ namespace CnBlogPublishTool
 
         public static string Upload(string filePath)
         {
-            FileInfo fileinfo = new FileInfo(filePath);
-            var mediaObjectInfo = BlogClient.NewMediaObject(fileinfo.Name, MimeMapping.GetMimeMapping(filePath), File.ReadAllBytes(filePath));
-            return mediaObjectInfo.URL;
+            var policy = Policy.Handle<Exception>().Retry(3,(exception, retryCount) =>
+            {
+                Console.WriteLine("上传失败，正在重试 {0}，异常：{1}", retryCount,exception.Message);
+            });
+            try
+            {
+                var url = policy.Execute<string>(() =>
+                {
+                    FileInfo fileinfo = new FileInfo(filePath);
+                    var mediaObjectInfo = BlogClient.NewMediaObject(fileinfo.Name, MimeMapping.GetMimeMapping(filePath), File.ReadAllBytes(filePath));
+                    return mediaObjectInfo.URL;
+                });
+
+                return url;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("上传失败,异常：{0}",e.Message);
+                throw;
+            }
         }
     }
 }
