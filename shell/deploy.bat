8f11ade80088db0ce559607f3a33fd97d6ca6baa
stulzq <stulzq@qq.com>
2020-11-27 10:20:46 +0800

    重命名

--- shell/deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set sendtopath=C:\Users\%username%\AppData\Roaming\Microsoft\Windows\SendTo
-
-move ..\Publish\dotnet-cnblog.exe.lnk %sendtopath%\dotnet-cnblog.lnk
-
-pause
--- shell/publish.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Mission preparation..
-
-dotnet publish ..\CnBlogPublishTool.sln  -c Release -r win10-x86 -o ../Publish
-
-echo mission accomplished!
-
-pause
-
