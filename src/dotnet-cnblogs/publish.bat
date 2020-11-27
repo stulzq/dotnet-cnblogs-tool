48e7c4f1d2ba7e544b5e32cfabfc8761cf013d80
stulzq <stulzq@qq.com>
2020-11-27 16:31:52 +0800

    修改发布配置

--- src/dotnet-cnblogs/publish.bat
+++ src/dotnet-cnblogs/publish.bat
@@ -1,5 +1,6 @@
-dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true
-dotnet publish -r win-x86 -p:PublishSingleFile=true --self-contained true
-dotnet publish -r osx-x64 -p:PublishSingleFile=true --self-contained true
-dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true
-dotnet publish -r linux-arm -p:PublishSingleFile=true --self-contained true
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true
+dotnet publish -c Release -r win-x86 -p:PublishSingleFile=true --self-contained true
+dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true --self-contained true
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
+dotnet publish -c Release -r linux-arm -p:PublishSingleFile=true --self-contained true
+pause
